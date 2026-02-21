489fc62891f4ea938c067e8b39fd638f1dbebeff
nameispawankalyan <pawankalyan4241@gmail.com>
2026-02-21 09:26:32 +0530

    Fixed Payload error

--- backend/index.js
+++ backend/index.js
@@ -34,7 +34,8 @@ app.use((req, res, next) => {
     next();
 });
 
-app.use(express.json());
+app.use(express.json({ limit: '100mb' }));
+app.use(express.urlencoded({ extended: true, limit: '100mb' }));
 
 const uploadDir = path.join(__dirname, 'storage_uploads');
 if (!fs.existsSync(uploadDir)) {
